3405a09ac442ccab8ac1f063fc48287111831df2
Aaryan09-coder <aaryanwaghmare2004@gmail.com>
2025-01-09 14:23:18 +0530

    Initail Commit

---
+++ src/main/java/com/telusko/SpringSecEx/StudentController.java
@@ -0,0 +1,33 @@
+package com.telusko.SpringSecEx;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Aaryan", 60),
+            new Student(2, "Manoj", 65)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public Student addStudents(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
+}
---
+++ src/main/java/com/telusko/SpringSecEx/UserRepo.java
@@ -0,0 +1,10 @@
+package com.telusko.SpringSecEx;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
